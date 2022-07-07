c7db4f1abfdad41f682393ac3f6ab6b2fe988a7b
mariorazo97 <mariorazo97@gmail.com>
2022-07-07 15:25:13 -0500

    1 file modified

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>1st Grade 40 single digit addition and subtraction per minute</p>
+      <p>1st Grade 50 single digit addition and subtraction per minute</p>
       <p>2nd Grade 70 single digit addition and subtraction per minute</p>
       <p>3rd Grade 110 single digit addition and subtraction per minute</p>
     </div>
