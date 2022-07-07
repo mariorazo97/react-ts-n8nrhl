f943b5ed492568aaff8be912b019fb303f1c5242
mariorazo97 <mariorazo97@gmail.com>
2022-07-07 15:13:35 -0500

    Initial commit

---
+++ mathfactsApp.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './mathfactsStyle.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>1st Grade 40 single digit addition and subtraction per minute</p>
+      <p>2nd Grade 70 single digit addition and subtraction per minute</p>
+      <p>3rd Grade 110 single digit addition and subtraction per minute</p>
+    </div>
+  );
+}
